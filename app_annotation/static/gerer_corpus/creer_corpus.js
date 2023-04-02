77cd2560d51ea4add5af5cc5c853617ddb3a6253
yassinekero <126423905+yassinekero@users.noreply.github.com>
2023-04-02 00:51:10 +0000

    Update creer_corpus.js

--- app_annotation/static/gerer_corpus/creer_corpus.js
+++ app_annotation/static/gerer_corpus/creer_corpus.js
@@ -1,19 +1,20 @@
 let plus = document.querySelector(".plus")
 let tweets = document.querySelector(".tweets")
-console.log("H")
 
+let id = 0
 
 function add()
 {
-  let id = makeid(15)
+  
     let tweet = document.createElement("div");
     tweet.className = "entry"
     tweet.id = id
     tweet.innerHTML = `
-     <span>Le Tweet: </span>
+     <span>Tweet: </span>
     <input type="text" name="tweet" class="fields tweet_fields" required>
     <button id="${id}"class="minus_btn add_remove" type="button" onclick="remove(this.id)">-</button>`
      tweets.appendChild(tweet)
+     id ++;
 }
 
 function remove(id)
@@ -24,22 +25,3 @@ function remove(id)
 
 
 
-
-function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-}
-
-
-let not = document.querySelector(".notif")
-    function close()
-    {
-    not.style.display = "none"
-    }
